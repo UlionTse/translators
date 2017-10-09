bfd372a2644a011c0877467d881b1462ddbbbb5a
UlionTse <shinalone@outlook.com>
2017-10-10 00:22:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+
 PACKAGE = "translate_api"
 NAME = "translate_api"
-DESCRIPTION = "Translate API for Python3 used Google.The default Google server for China, you can change."
+DESCRIPTION = "Translate API for Python3 used Google.The default Google server for China, you can switch to use."
 AUTHOR = "UlionTse"
 AUTHOR_EMAIL = "shinalone@outlook.com"
 URL = "https://github.com/shinalone/translate_api"
@@ -24,13 +25,20 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Development Status :: 2 - Production/stable",
+        "Development Status :: 5 - Production/stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
+    ],
+    keywords=['translate','translate_api'],
+    python_requires='>=3',
+    install_requires=[
+        'requests>=2.9.1',
+        'PyExecJS>=1.2.0',
+        'fake-useragent>=0.1.7'
     ],
     zip_safe=False,
 )
