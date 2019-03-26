1cc49f68336305c3ba4d1a117a2fd5d8d61af310
UlionTse <shinalone@outlook.com>
2019-03-27 00:52:35 +0800

    V4.0.2
    V4.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# coding:utf-8
+# coding=utf-8
+# author=uliontse
 
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup,find_packages
 
 
 PACKAGE = "translators"
@@ -12,7 +12,7 @@ AUTHOR_EMAIL = "shinalone@outlook.com"
 URL = "https://github.com/shinalone/translators"
 VERSION = __import__(PACKAGE).__version__
 
-with open('README.rst','r',encoding='utf-8') as file:
+with open('README.md','r',encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
@@ -20,6 +20,7 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="MIT",
@@ -27,16 +28,16 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.4",
+        # "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
     ],
-    keywords=['translate','translators','translator','fanyi','Google','Youdao','NetEase'],
+    keywords=['translators','translate','translator','fanyi','Google','Youdao','NetEase'],
     install_requires=[
         'requests>=2.9.1',
         'PyExecJS>=1.2.0'
