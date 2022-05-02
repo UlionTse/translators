63f42465a41059e6cddc57e8ff8d47d5641fea6a
UlionTse <shinalone@outlook.com>
2022-05-03 02:11:52 +0800

    upgrade v5.1.1

--- translators/__init__.py
+++ translators/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.1.0"
+__version__ = "5.1.1"
 __author__ = "UlionTse"
 
 from translators.apis import (alibaba, argos, baidu, bing, caiyun, deepl, google, iciba, iflytek, itranslate,
