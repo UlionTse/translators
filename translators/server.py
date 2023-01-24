cccfbe5e59f2d58fa60160ead7b17d0880f6aead
UlionTse <shinalone@outlook.com>
2023-01-24 22:53:04 +0800

    upgrade v5.5.6

--- translators/server.py
+++ translators/server.py
@@ -1347,12 +1347,13 @@ class Bing(Tse):
 
     def get_ig_iid(self, host_html):
         et = lxml.etree.HTML(host_html)
-        iid = et.xpath('//*[@id="rich_tta"]/@data-iid')[0]
+        # iid = et.xpath('//*[@id="tta_outGDCont"]/@data-iid')[0]  # browser page is different between request page.
+        iid = 'translator.5028'
         ig = re.compile('IG:"(.*?)"').findall(host_html)[0]
         return {'iid': iid, 'ig': ig}
 
     def get_tk(self, host_html):
-        result_str = re.compile('var params_RichTranslateHelper = (.*?);').findall(host_html)[0]
+        result_str = re.compile('var params_AbusePreventionHelper = (.*?);').findall(host_html)[0]
         result = execjs.eval(result_str)
         return {'key': result[0], 'token': result[1]}
 
@@ -1402,15 +1403,16 @@ class Bing(Tse):
         from_language, to_language = self.check_language(from_language, to_language, self.language_map,
                                                          output_zh=self.output_zh, output_auto=self.output_auto)
 
-        form_data = {**{'text': query_text, 'fromLang': from_language, 'to': to_language}, **self.tk}
-        api_url_param = f'?isVertical=1&&IG={self.ig_iid["ig"]}&IID={self.ig_iid["iid"]}.{self.query_count + 1}'
+        form_data = {'text': query_text, 'fromLang': from_language, 'to': to_language, 'tryFetchingGenderDebiasedTranslations': 'true'}
+        form_data = {**form_data, **self.tk}
+        api_url_param = f'?isVertical=1&&IG={self.ig_iid["ig"]}&IID={self.ig_iid["iid"]}'
         api_url = ''.join([self.api_url, api_url_param])
         r = self.session.post(api_url, headers=self.host_headers, data=form_data, timeout=timeout, proxies=proxies)
         r.raise_for_status()
         data = r.json()
         time.sleep(sleep_seconds)
         self.query_count += 1
-        return data if is_detail_result else data[0]['translations'][0]['text']
+        return data[0] if is_detail_result else data[0]['translations'][0]['text']
 
 
 class Sogou(Tse):
@@ -2739,7 +2741,7 @@ class Lingvanex(Tse):
         data = r.json()
         time.sleep(sleep_seconds)
         self.query_count += 1
-        return data if is_detail_result else data['result'] if self.mode == 'B2C' else data['result']['text']
+        return data if is_detail_result else data['result']['text']
 
 
 class Niutrans(Tse):
@@ -2913,6 +2915,114 @@ class Mglip(Tse):
         time.sleep(sleep_seconds)
         self.query_count += 1
         return data if is_detail_result else data['datas'][0]['paragraph'] if data['datas'][0]['type'] == 'trans' else data['datas'][0]['data']
+
+
+class Volcengine(Tse):
+    def __init__(self):
+        super().__init__()
+        self.host_url = 'https://translate.volcengine.com'
+        self.api_url = 'https://translate.volcengine.com/web/translate/v1'
+
+
+        self.language_url = None
+
+        self.host_headers = self.get_headers(self.host_url, if_api=False)
+        self.api_headers = self.get_headers(self.host_url, if_api=True, if_json_for_api=False)
+        self.session = None
+        self.language_map = None
+
+        self.query_count = 0
+        self.output_zh = 'zh'
+        self.input_limit = 5000
+
+    @Tse.debug_language_map
+    def get_language_map(self, lang_url, ss, headers, timeout, proxies, **kwargs):
+        params = {'all': 'true', 'code': 'en_GB', 'platform': 'dp', '_': int(time.time() * 1000)}
+        detail_lang_map = ss.get(lang_url, params=params, headers=headers, timeout=timeout, proxies=proxies).json()
+        for _ in range(3):
+            _ = ss.get(lang_url, params={'platform': 'dp'}, headers=headers, timeout=timeout, proxies=proxies)
+        lang_list = sorted(set([item['full_code'] for item in detail_lang_map['result']]))
+        return {}.fromkeys(lang_list, lang_list)
+
+    def get_d_lang_map(self, lang_url, ss, headers, timeout, proxies):
+        params = {'all': 'true', 'code': 'en_GB', 'platform': 'dp', '_': int(time.time() * 1000)}
+        return ss.get(lang_url, params=params, headers=headers, timeout=timeout, proxies=proxies).json()
+
+    def get_auth(self, auth_url, ss, headers, timeout, proxies):
+        js_html = ss.get(auth_url, headers=headers, timeout=timeout, proxies=proxies).text
+        return {k: v for k, v in re.compile(',(.*?)="(.*?)"').findall(js_html)}
+
+    @Tse.time_stat
+    @Tse.check_query
+    def lingvanex_api(self, query_text: str, from_language: str = 'auto', to_language: str = 'en', **kwargs) -> Union[str, dict]:
+        """
+        https://lingvanex.com/demo/
+        :param query_text: str, must.
+        :param from_language: str, default 'auto'.
+        :param to_language: str, default 'zh'.
+        :param **kwargs:
+                :param timeout: float, default None.
+                :param proxies: dict, default None.
+                :param sleep_seconds: float, default `random.random()`.
+                :param is_detail_result: boolean, default False.
+                :param if_ignore_limit_of_length: boolean, default False.
+                :param limit_of_length: int, default 5000.
+                :param if_ignore_empty_query: boolean, default False.
+                :param update_session_after_seconds: float, default 1500.
+                :param if_show_time_stat: boolean, default False.
+                :param show_time_stat_precision: int, default 4.
+                :param lingvanex_mode: str, default "B2C", choose from ("B2B", "B2C").
+        :return: str or dict
+        """
+
+        mode = kwargs.get('lingvanex_mode', 'B2C')
+        timeout = kwargs.get('timeout', None)
+        proxies = kwargs.get('proxies', None)
+        is_detail_result = kwargs.get('is_detail_result', False)
+        sleep_seconds = kwargs.get('sleep_seconds', random.random())
+        update_session_after_seconds = kwargs.get('update_session_after_seconds', self.default_session_seconds)
+
+        if mode not in self.model_pool:
+            raise TranslatorError
+
+        not_update_cond_time = 1 if time.time() - self.begin_time < update_session_after_seconds else 0
+        if not (self.session and not_update_cond_time and self.language_map and self.auth_info and self.mode == mode):
+            self.session = requests.Session()
+            _ = self.session.get(self.host_url, headers=self.host_headers, timeout=timeout, proxies=proxies)
+            self.auth_info = self.get_auth(self.auth_url, self.session, self.host_headers, timeout, proxies)
+
+            if mode != self.mode:
+                self.mode = mode
+                self.api_url = ''.join([self.auth_info[f'{mode}_BASE_URL'], self.auth_info['TRANSLATE_URL']])
+                self.language_url = ''.join([self.auth_info[f'{mode}_BASE_URL'], self.auth_info['GET_LANGUAGES_URL']])
+                self.host_headers.update({'authorization': self.auth_info[f'{mode}_AUTH_TOKEN']})
+                self.api_headers.update({'authorization': self.auth_info[f'{mode}_AUTH_TOKEN']})
+                self.api_headers.update({'referer': urllib.parse.urlparse(self.auth_info[f'{mode}_BASE_URL']).netloc})
+
+            self.language_map = self.get_language_map(self.language_url, self.session, self.host_headers, timeout, proxies,
+                                                      from_language=from_language, to_language=to_language)
+            self.detail_language_map = self.get_d_lang_map(self.language_url, self.session, self.host_headers, timeout, proxies)
+
+        if from_language == 'auto':
+            warnings.warn('Unsupported [from_language=auto] with [lingvanex]! Please specify it.')
+            from_language = self.output_zh
+        from_language, to_language = self.check_language(from_language, to_language, self.language_map, output_zh=self.output_zh,
+                                                         output_en_translator='Lingvanex', output_en='en-US')
+
+        form_data = {
+            'from': from_language,
+            'to': to_language,
+            'text': query_text,
+            'platform': 'dp',
+            'is_return_text_split_ranges': 'true'
+        }
+        form_data = urllib.parse.urlencode(form_data)
+        r = self.session.post(self.api_url, data=form_data, headers=self.api_headers, timeout=timeout, proxies=proxies)
+        r.raise_for_status()
+        data = r.json()
+        time.sleep(sleep_seconds)
+        self.query_count += 1
+        return data if is_detail_result else data['result']['text']
 
 
 class TranslatorsServer:
